42c492335891e43ead94fe68994728641ef06cb1
ilangrin <152694435+ilangrin@users.noreply.github.com>
2023-12-27 11:32:22 +0200

    tested

--- test_synaps_stg.py
+++ test_synaps_stg.py
@@ -11,8 +11,22 @@ import pytest
 
 
 class TestLoginSanity:
+    def find_and_interact(self, driver, wait, button_xpath, textarea_xpath=None, next_text=None):
+        """
+        Helper method to find and click a button, and optionally interact with a textarea.
+        """
+        button = wait.until( EC.element_to_be_clickable( (By.XPATH, button_xpath) ) )
+        button.click()
 
-
+        if textarea_xpath and next_text is not None:
+            try:
+                textarea = wait.until( EC.element_to_be_clickable( (By.XPATH, textarea_xpath) ) )
+                textarea.clear()
+                textarea.send_keys( next_text )
+            except StaleElementReferenceException:
+                textarea = driver.find_element( By.XPATH, textarea_xpath )
+                textarea.clear()
+                textarea.send_keys( next_text )
 
     def get_next_name(self):
         global counter1, counter2, name_index
@@ -35,100 +49,71 @@ class TestLoginSanity:
 
         return name
 
-
-
     @pytest.fixture()
     def setup(self):
         driver = webdriver.Chrome()
-        driver.get( 'https://synaps-stg-aab67ad5805a.herokuapp.com/Signin')
+        driver.get( 'https://synaps-stg-aab67ad5805a.herokuapp.com/Signin' )
         driver.maximize_window()
         driver.implicitly_wait( 10 )
         yield driver
         driver.close()
 
-
-
     def test_login_sanity_test(self, setup):
         driver = setup
-
 
         actions = ActionChains( driver )
         wait = WebDriverWait( driver, 5 )
         wait3 = WebDriverWait( driver, 3 )
-        driver.find_element( By.XPATH, '//*[@id="mobile-modal"]/div/div[2]/div[2]/div[1]/div/button' ).click()
-        time.sleep( 2 )
-        driver.find_element( By.XPATH, '//*[@id="mobile-modal"]/div/div[2]/div[4]/div/input' ).send_keys("test08@synaps.co" )
+        self.find_and_interact( driver, wait, '//*[@id="mobile-modal"]/div/div[2]/div[2]/div[1]/div/button' )
+        driver.find_element( By.XPATH, '//*[@id="mobile-modal"]/div/div[2]/div[4]/div/input' ).send_keys(
+            "test08@synaps.co" )
         driver.find_element( By.XPATH, '//*[@id="mobile-modal"]/div/div[2]/div[5]/div/input' ).send_keys( "123456" )
-        checkbox = wait.until( EC.element_to_be_clickable( (By.XPATH, '//*[@id="default-checkbox"]') ) )
-        checkbox.click()
-#תחילת תהליך בדיקה
+        self.find_and_interact( driver, wait, '//*[@id="default-checkbox"]' )
 
+        # תחילת תהליך בדיקה
 
         # Click the login button
-        login_submit_button = wait.until(EC.element_to_be_clickable( (By.XPATH, '//*[@id="mobile-modal"]/div/div[2]/div[7]/button') ) )
-        login_submit_button.click()
+        self.find_and_interact( driver, wait, '//*[@id="mobile-modal"]/div/div[2]/div[7]/button' )
 
-        try:
+        self.find_and_interact( driver, wait, '/html/body/div[3]/div/div/div[1]/button')
 
-            selection_page_element = wait.until(EC.visibility_of_element_located( (By.XPATH, '/html/body/div[3]/div/div/div[1]/button') ) )
-            selection_page_element.click()
-        except TimeoutException:
-
-            print( "avatar page not found, continuing with the script." )
-
-        try:
-            WebDriverWait( driver, 5 ).until(EC.visibility_of_element_located( (By.CLASS_NAME, "ReactModal__Overlay") ) )
-
-        except TimeoutException:
-            print( "The ReactModal__Overlay element did not appear within 5 seconds." )
-
-        button = driver.find_element( By.XPATH, '//*[@id="mobile-modal"]/div/div[2]/div[3]/div[2]/div[2]/div/button' )
-        button.click()
-        next_element_xpath = '//*[@id="mobile-modal"]/div/div[2]/div[3]/div[2]/div[2]/p[1]'
-        wait.until( EC.visibility_of_element_located( (By.XPATH, next_element_xpath) ) )
+        self.find_and_interact( driver, wait, '//*[@id="mobile-modal"]/div/div[2]/div[3]/div[2]/div[2]/div/button' )
+        self.find_and_interact( driver, wait, '//*[@id="mobile-modal"]/div/div[2]/div[3]/div[2]/div[2]/p[1]' )
         element = driver.find_element( By.XPATH, '//*[@id="mobile-modal"]/div/div[2]/div[3]/div[2]/div[2]/p[1]' )
         driver.execute_script( "arguments[0].scrollIntoView(true);", element )
-        #בחירת אתגר
-        driver.find_element( By.XPATH, '//*[@id="mobile-modal"]/div/div[2]/div[3]/div[2]/div[2]/div/button' ).click()
+
+        # בחירת אתגר
+
+        self.find_and_interact( driver, wait, '//*[@id="mobile-modal"]/div/div[2]/div[3]/div[2]/div[2]/div/button' )
         element_challenge_info = driver.find_element( By.ID, "challenge_info" )
         driver.execute_script( "arguments[0].click();", element_challenge_info )
         time.sleep( 4 )
-        driver.find_element( By.XPATH, '//*[@id="mobile-modal"]/div/div[2]/div[7]/div/div/button' ).click()
+        self.find_and_interact( driver, wait, '//*[@id="mobile-modal"]/div/div[2]/div[7]/div/div/button' )
         element_brainwrite = driver.find_element( By.ID, "brain_write" )
         driver.execute_script( "arguments[0].click();", element_brainwrite )
         time.sleep( 4 )
 
-        for _ in range( 5 ):  # Replace with the number of times you want to repeat
-            # Click the button
-            button = wait.until( EC.element_to_be_clickable( (By.XPATH, '//*[@id="mobile-modal"]/div/div[2]/div[4]/div/div/button') ) )
-            button.click()
-            # Interact with the textarea
-            try:
-                textarea = wait.until(
-                    EC.element_to_be_clickable( (By.XPATH, '//*[@id="mobile-modal"]/div/div[2]/div[4]/textarea') ) )
-                next_text = self.get_next_name()
-                textarea.clear()
-                textarea.send_keys( next_text )
-            except StaleElementReferenceException:
-                # Re-find the textarea element
-                textarea = driver.find_element( By.XPATH, '//*[@id="mobile-modal"]/div/div[2]/div[4]/textarea' )
-                textarea.clear()
-                textarea.send_keys( next_text )
+        for _ in range( 5 ):
+            next_text = self.get_next_name()
+            self.find_and_interact(
+                driver, wait,
+                '//*[@id="mobile-modal"]/div/div[2]/div[4]/div/div/button',
+                '//*[@id="mobile-modal"]/div/div[2]/div[4]/textarea',
+                next_text )
+
+            self.find_and_interact( driver, wait, "//*[contains(text(), 'הוסף רעיון')]" )
 
 
-            link = wait.until( EC.element_to_be_clickable( (By.XPATH, "//*[contains(text(), 'הוסף רעיון')]") ) )
-            link.click()
-#בחירת רעיון טוב
-        bestIdea = driver.find_element(By.XPATH,'//*[@id="mobile-modal"]/div/div[2]/div[2]/div[2]')
-        bestIdea.click()
-        Justify = driver.find_element(By.XPATH,'//*[@id="mobile-modal"]/div/div[2]/div[1]/div[2]/textarea')
-        Justify.send_keys("test")
-
-        time.sleep(10)
+        # בחירת רעיון טוב
+        good_next_text = self.get_next_name + "good"()
+        self.find_and_interact( driver, wait, '' )
+        self.find_and_interact(
+            driver, wait,
+            '//*[@id="mobile-modal"]/div/div[2]/div[2]/div[2]',
+            '//*[@id="mobile-modal"]/div/div[2]/div[1]/div[2]/textarea', good_next_text )
 
 
 
-
-
+        time.sleep( 10 )
 
         driver.quit()
