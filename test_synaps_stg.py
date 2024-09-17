ac1224170afb99feedd8bb78f9ae0385af00ce90
ilangrin <152694435+ilangrin@users.noreply.github.com>
2024-09-17 14:36:18 +0300

    17.9

--- test_synaps_stg.py
+++ test_synaps_stg.py
@@ -8,7 +8,6 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.common.exceptions import TimeoutException
 counter1, counter2, name_index = 1, 1, 0
 
 class TestLoginSanity:
